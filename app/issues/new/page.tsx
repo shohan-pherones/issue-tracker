e3d8425da422f412c8b17efcc9126d4cc293c9f1
Md. Shohanur Rahman <shohan.sub.56@gmail.com>
2023-10-18 17:04:08 +0600

    spinner

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -11,6 +11,7 @@ import { zodResolver } from "@hookform/resolvers/zod";
 import { createIssueSchema } from "@/app/validationSchemas";
 import { z } from "zod";
 import ErrorMessage from "@/app/components/ErrorMessage";
+import Spinner from "@/app/components/Spinner";
 
 type IssueForm = z.infer<typeof createIssueSchema>;
 
@@ -24,7 +25,8 @@ const NewIssuePage = () => {
   } = useForm<IssueForm>({
     resolver: zodResolver(createIssueSchema),
   });
-  const [error, setError] = useState("");
+  const [error, setError] = useState<string>("");
+  const [isSubmitting, setIsSubmitting] = useState<boolean>(false);
 
   return (
     <div className="max-w-xl">
@@ -36,10 +38,13 @@ const NewIssuePage = () => {
       <form
         onSubmit={handleSubmit(async (data) => {
           try {
+            setIsSubmitting(true);
             await axios.post("/api/issues", data);
             router.push("/issues");
           } catch (error) {
             setError("An unexpected error occurred.");
+          } finally {
+            setIsSubmitting(false);
           }
         })}
         className="max-w-xl space-y-3"
@@ -56,7 +61,10 @@ const NewIssuePage = () => {
           )}
         />
         <ErrorMessage>{errors.description?.message}</ErrorMessage>
-        <Button>Submit New Issue</Button>
+        <Button disabled={isSubmitting}>
+          {isSubmitting && <Spinner />} {isSubmitting ? "Submitting" : "Submit"}{" "}
+          New Issue
+        </Button>
       </form>
     </div>
   );
