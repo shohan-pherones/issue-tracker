6ef10f85dc654fa4aee2c9b2f561d01d35a24bc5
Md. Shohanur Rahman <shohan.sub.56@gmail.com>
2023-10-18 19:07:56 +0600

    skeleton loading

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,13 +1,11 @@
 import prisma from "@/prisma/client";
 import { Table } from "@radix-ui/themes";
 import IssueStatusBadge from "../components/IssueStatusBadge";
-import delay from "delay";
-import IssueActions from "./IssueActions";
 import Link from "../components/Link";
+import IssueActions from "./IssueActions";
 
 const IssuesPage = async () => {
   const issues = await prisma.issue.findMany();
-  await delay(2000);
 
   return (
     <div>
