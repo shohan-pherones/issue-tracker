69525c64b44c895a76943d08b44d2ad794acd3c3
Md. Shohanur Rahman <shohan.sub.56@gmail.com>
2023-10-19 02:09:11 +0600

    assignee api

--- app/issues/[id]/AssigneeSelect.tsx
+++ app/issues/[id]/AssigneeSelect.tsx
@@ -1,15 +1,40 @@
 "use client";
 
-import { Select } from "@radix-ui/themes";
+import { User } from "@prisma/client";
+import { Avatar, Select } from "@radix-ui/themes";
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 const AssigneeSelect = () => {
+  const [users, setUsers] = useState<User[]>([]);
+
+  useEffect(() => {
+    const fetchUsers = async () => {
+      const { data } = await axios.get<User[]>("/api/users");
+      setUsers(data);
+    };
+
+    fetchUsers();
+  }, []);
+
   return (
     <Select.Root>
       <Select.Trigger placeholder="Assign..." />
       <Select.Content>
         <Select.Group>
           <Select.Label>Suggestions</Select.Label>
-          <Select.Item value="1">Natasha</Select.Item>
+          {users.map((user) => (
+            <Select.Item key={user.id} value={user.id}>
+              <Avatar
+                src={user.image!}
+                fallback="?"
+                size="1"
+                radius="full"
+                referrerPolicy="no-referrer"
+              />{" "}
+              {user.name}
+            </Select.Item>
+          ))}
         </Select.Group>
       </Select.Content>
     </Select.Root>
