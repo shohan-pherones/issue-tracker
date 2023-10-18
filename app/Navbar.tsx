3947dc4d3d3901151e2eb5aa74f1e608a105afb0
Md. Shohanur Rahman <shohan.sub.56@gmail.com>
2023-10-19 01:27:13 +0600

    login skeleton

--- app/Navbar.tsx
+++ app/Navbar.tsx
@@ -13,6 +13,7 @@ import { useSession } from "next-auth/react";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { AiFillBug } from "react-icons/ai";
+import { Skeleton } from "@/app/components";
 
 const Navbar = () => {
   return (
@@ -68,7 +69,7 @@ const NavLinks = () => {
 const AuthStatus = () => {
   const { status, data: session } = useSession();
 
-  if (status === "loading") return null;
+  if (status === "loading") return <Skeleton width="3rem" />;
 
   if (status === "unauthenticated")
     return (
