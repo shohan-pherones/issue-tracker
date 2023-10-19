e44910146fc4f8766c52438137240db95d37ba8b
Md. Shohanur Rahman <shohan.sub.56@gmail.com>
2023-10-19 15:19:40 +0600

    issue summary

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,14 @@
-import LatestIssues from "./LatestIssues";
+import prisma from "@/prisma/client";
+import IssueSummary from "./IssueSummary";
 
-const HomePage = () => {
-  return <LatestIssues />;
+const HomePage = async () => {
+  const open = await prisma.issue.count({ where: { status: "OPEN" } });
+  const inProgress = await prisma.issue.count({
+    where: { status: "IN_PROGRESS" },
+  });
+  const closed = await prisma.issue.count({ where: { status: "CLOSED" } });
+
+  return <IssueSummary open={open} closed={closed} inProgress={inProgress} />;
 };
 
 export default HomePage;
